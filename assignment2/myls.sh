b216ab6428ca612347d91de3426971a3a2da5823
tarikwaleed <takizee53@gmail.com>
2022-12-18 15:39:25 +0200

    done with problem5

--- assignment2/myls.sh
+++ assignment2/myls.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ $* == 0 ]];then
+ls
 # ls $HOME # didnt' work
 # eval "ls ~" #didn't work too
 # ls ~ #didn't work too
 # ls /home/tarikwaleed #didn't work too
+
 else
 ls $1
 fi
